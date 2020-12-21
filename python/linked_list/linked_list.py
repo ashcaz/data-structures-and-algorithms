0d59f6580eb5833608277bb2c29140b98c6ca3cb
Ashley Casimir <ashcas@me.com>
2020-12-20 16:55:33 -0800

    test_instantiate_empty_ll passed

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,28 @@
+class Node:
+
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class LinkedList:
-    """
-    Put docstring here
-    """
+    instances = 0
 
-    def __init__(self):
-        # initialization here
+    def __init__(self, head, values=None):
+        self.head = head
+        LinkedList.instances +=1
+
+
+    def insert(self, value):
+        node = Node(value) # value 8 next None
+
+        if self.head is not None:
+            node.next = self.head
+        self.head = node
+
+    def includes(self):
         pass
 
-    def some_method(self):
-        # method body here
-        pass
+    @classmethod
+    def ll_total(cls):
+        return cls.instances
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,70 @@
-from linked_list.linked_list import LinkedList
+import pytest
+
+from linked_list.linked_list import Node, LinkedList
 
 
 def test_import():
     assert LinkedList
+
+# @pytest.mark.skip("pending")
+def test_instantiate_empty_ll():
+  node = Node(1)
+  LinkedList(node)
+  actual = LinkedList.ll_total()
+  expected = 1
+  assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_insert_into_ll():
+  node = Node(1)
+  new_ll = LinkedList(node)
+  actual = new_ll.insert(2)
+  expected = -1
+  assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_head_ll():
+  node = Node(1)
+  actual = LinkedList(node)
+  expected = -1
+  assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_insert_multiple_nodes_ll():
+  node = Node(1)
+  actual = LinkedList(node)
+  expected = -1
+  assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_value_found_in_ll():
+  node = Node(1)
+  actual = LinkedList(node)
+  expected = -1
+  assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_value_not_found_in_ll():
+  node = Node(1)
+  actual = LinkedList(node)
+  expected = -1
+  assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_return_collection_of_values():
+  node = Node(1)
+  actual = LinkedList(node)
+  expected = -1
+  assert actual == expected
+
+
+#######################
+# Fixtures
+#######################
+@pytest.fixture(autouse=True)
+def clean():
+    """runs before each test automatically
+    There's also a more advanced way to run code after each test as well
+    Check the docs for that. Hint: it uses yield
+    """
+    LinkedList.instances = 0
