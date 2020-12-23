a37e41326affff43d165599e076f2f14661645e7
Ashley Casimir <ashcas@me.com>
2020-12-22 20:51:08 -0800

    updated README

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,15 +1,78 @@
+class Node:
+
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class LinkedList:
     """
-    Put docstring here
+    Liked List Class
     """
+    instances = 0
 
-    def __init__(self):
-        # initialization here
+    def __init__(self, head=None):
+        self.head = head
+        
+        if self.head == None:
+            self.length = 0
+        else:
+            self.length = 1
+
+        LinkedList.instances+=1
+
+    def __str__(self):
+        dunder_str = ''
+        current = self.head
+
+        if current == None:
+            return 'This is an empty Link List'
+
+        while current is not None:
+            dunder_str += f'{ {current.value} } -> '
+            current = current.next
+        
+        dunder_str += f'NULL'
+
+        return dunder_str
+
+    def insert(self, value):
+        node = Node(value)
+
+        if self.head is not None:
+            node.next = self.head
+        self.head = node
+        self.length+=1
+
+    def includes(self, search_value) -> bool:
+        current = self.head
+        while current is not None:
+            if current.value == search_value:
+                return True
+            current = current.next
+        return False
+
+    def create_collection(self)-> list:
+        collection =[]
+        current = self.head
+
+        while current is not None:
+            collection.append(current.value)
+            current = current.next
+
+        return collection
+
+    def append(self, val):
         pass
 
-<<<<<<< Updated upstream
-    def some_method(self):
-        # method body here
+    def insertBefore(self, search_val,new_value):
         pass
-=======
->>>>>>> Stashed changes
+
+    def insertBefore(self, search_val,new_value):
+        pass
+
+    @classmethod
+    def linked_lists_total(cls):
+        return cls.instances
+
+
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,93 @@
-from linked_list.linked_list import LinkedList
+import pytest
+
+from linked_list.linked_list import Node, LinkedList
 
 
 def test_import():
     assert LinkedList
+
+# @pytest.mark.skip("pending")
+def test_instantiate_empty_ll():
+  empty_linked_list = LinkedList()
+  actual = empty_linked_list.length
+  expected = 0
+  assert actual == expected
+
+# @pytest.mark.skip("pending")
+def test_insert_into_ll(long_linked_list):
+  length = long_linked_list.length
+  long_linked_list.insert(22)
+  actual = long_linked_list.length
+  expected = length+1
+  assert actual == expected
+
+# @pytest.mark.skip("pending")
+def test_head_ll(short_linked_list):
+  short_linked_list.insert(27)
+  actual = short_linked_list.head.value
+  expected = 27
+  assert actual == expected
+  
+
+# @pytest.mark.skip("pending")
+def test_insert_multiple_nodes_ll():
+  node = Node(1)
+  ll = LinkedList(node)
+  ll.insert(2)
+  ll.insert(25)
+  ll.insert(102)
+  actual = ll.length
+  expected = 4
+  assert actual == expected
+
+# @pytest.mark.skip("pending")
+def test_value_found_in_ll(long_linked_list):
+  actual = long_linked_list.includes(1)
+  expected = True
+  assert actual == expected
+
+# @pytest.mark.skip("pending")
+def test_value_not_found_in_ll(long_linked_list):
+  actual = long_linked_list.includes(22)
+  expected = False
+  assert actual == expected
+
+# @pytest.mark.skip("pending")
+def test_return_collection_of_values(short_linked_list):
+  actual = short_linked_list.create_collection()
+  expected = [2,1,0]
+  assert actual == expected
+
+
+#######################
+# Fixtures
+#######################
+
+@pytest.fixture
+def long_linked_list():
+    node = Node(0)
+    linked_list = LinkedList(node)
+    node_values = list(range(1,10))
+    for num in node_values:
+        linked_list.insert(num)
+    
+    return linked_list
+
+@pytest.fixture
+def short_linked_list():
+    node = Node(0)
+    linked_list = LinkedList(node)
+    node_values = list(range(1,3))
+    for num in node_values:
+        linked_list.insert(num)
+    
+    return linked_list
+
+
+@pytest.fixture(autouse=True)
+def clean():
+    """runs before each test automatically
+    There's also a more advanced way to run code after each test as well
+    Check the docs for that. Hint: it uses yield
+    """
+    LinkedList.instances = 0
