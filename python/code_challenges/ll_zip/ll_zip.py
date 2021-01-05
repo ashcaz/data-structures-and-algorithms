6e42363e0d7aaf8b2fa3ba61e1644b9aaae1bc27
Ashley Casimir <ashcas@me.com>
2021-01-04 21:49:53 -0800

    updated readme and created scaffold

---
+++ python/code_challenges/ll_zip/ll_zip.py
@@ -0,0 +1,11 @@
+from linked_list.linked_list import LinkedList, Node
+
+
+def zip_lists(list1, list2):
+  """[summary]
+
+  Args:
+      list1 ([type]): [description]
+      list2 ([type]): [description]
+  """
+  
