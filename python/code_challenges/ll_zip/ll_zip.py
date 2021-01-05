30ab9fe09605b223447ec69ddba26c82444fe085
Ashley Casimir <ashcas@me.com>
2021-01-04 22:06:56 -0800

    added code

--- python/code_challenges/ll_zip/ll_zip.py
+++ python/code_challenges/ll_zip/ll_zip.py
@@ -8,4 +8,23 @@ def zip_lists(list1, list2):
       list1 ([type]): [description]
       list2 ([type]): [description]
   """
-  
+  ll1_current = list1.head
+  ll2_current = list2.head
+  temp = ll2_current.next
+
+  while ll1_current is not None:
+    if ll1_current == None or ll2_current == None:
+      ll2_current.next = ll1_current.next
+      ll1_current.next = ll2_current
+      return  list1
+    else:
+      temp = temp.next
+      ll2_current.next = ll1_current.next
+      ll1_current.next = ll2_current
+
+      ll1_current = ll1_current.next.next
+      ll2_current = ll2_current.next
+
+  return list1
+
+      
