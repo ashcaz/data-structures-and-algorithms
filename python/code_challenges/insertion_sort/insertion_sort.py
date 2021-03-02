649e7a9223a18694ac0a838f4d8aaa4cedef6c16
Ashley Casimir <ashcas@me.com>
2021-03-01 20:59:53 -0800

    tests written

---
+++ python/code_challenges/insertion_sort/insertion_sort.py
@@ -0,0 +1,13 @@
+def sort_func(sort_list: list) -> list:
+
+    # FOR i = 1 to arr.length
+
+    #   var int j <-- i - 1
+    #   int temp <-- arr[i]
+
+    #   WHILE j >= 0 AND temp < arr[j]
+    #     arr[j + 1] <-- arr[j]
+    #     j <-- j - 1
+
+    #   arr[j + 1] <-- temp
+    return sort_list
